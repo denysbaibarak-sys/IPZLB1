63ad95e91118e7a88ac0bd0261ad89803a98160f
Denys Baibarak <denysbaibarak@gmail.com>
2026-03-29 22:51:09 +0300

    +

---
+++ WebApplication1/WebApplication1/Controllers/RestaurantController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Net;
+public class RestaurantController : ApiController
+{
+    //private static List<Restaurant> restaurants = new List<Restaurant>();
+    private FileService fileService = new FileService();
+
+    [HttpPost]
+    [Route("api/restaurants")]
+    public IHttpActionResult AddRestaurant(Restaurant restaurant)
+    {
+        if (restaurant == null)
+            return BadRequest("Restaurant is null");
+
+        var restaurants = fileService.LoadRestaurants();
+
+        restaurants.Add(restaurant);
+
+        fileService.SaveRestaurants(restaurants);
+
+        return StatusCode(HttpStatusCode.Created);
+    }
+
+    [HttpGet]
+    [Route("api/restaurants")]
+    public List<Restaurant> GetRestaurants()
+    {
+        return fileService.LoadRestaurants();
+    }
+}
